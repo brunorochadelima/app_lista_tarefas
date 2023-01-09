8a8151fe5e02da5a627dfde5f8a202583acbca1d
brunorochadelima <brunorocha33@live.com>
2023-01-09 13:47:54 -0300

    inserindo registros

--- app_lista_tarefas_private/tarefa_controler.php
+++ app_lista_tarefas_private/tarefa_controler.php
@@ -1,22 +1,25 @@
-<?php 
+<?php
 
 require "tarefa.model.php";
 require "conexao.php";
 require "tarefa.service.php";
 
-echo '<pre>';
-print_r($_POST);
-echo '</pre>';
+$acao = isset($_GET["acao"]) ? $_GET["acao"] : $acao;
 
-$tarefa = new Tarefa();
-$tarefa->__set('tarefa', $_POST['tarefa']);
+if ($acao == 'inserir') {
+    $tarefa = new Tarefa();
+    $tarefa->__set('tarefa', $_POST['tarefa']);
 
-$conexao = new Conexao();
+    $conexao = new Conexao();
 
-$tarefaService = new TarefaService($conexao, $tarefa);
-$tarefaService->inserir();
+    $tarefaService = new TarefaService($conexao, $tarefa);
+    $tarefaService->inserir();
 
+    header('Location: nova_tarefa.php?inclusao=1');
+} else if ($acao == 'recuperar') {
+    $tarefa = new Tarefa();
+    $conexao = new Conexao();
 
-echo '<pre>';
-print_r($tarefaService);
-echo '</pre>';
+    $tarefaService = new TarefaService($conexao, $tarefa);
+    $tarefas = $tarefaService->recuperar();
+} 
